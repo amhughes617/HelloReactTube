71807d8915b3d8153d7dd2158a4bde0c5273c2fe
amhughes617 <amhughes@g.cofc.edu>
2016-05-28 20:49:59 -0400

    Initial Import

--- src/index.js
+++ src/index.js
@@ -1,15 +1,12 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
+import React from 'react'; //es6 getting React; creates components
+import ReactDOM from 'react-dom'; //interacts with the DOM
 
-import App from './components/app';
-import reducers from './reducers';
+const API_KEY = 'AIzaSyAP3sOevtKXpR6Oxou8AxavG5e_HXsWdnM';
 
-const createStoreWithMiddleware = applyMiddleware()(createStore);
+// Create a new component to produce html.
+const App = () => {
+  return <div>Hi!</div>; //JSX
+}
 
-ReactDOM.render(
-  <Provider store={createStoreWithMiddleware(reducers)}>
-    <App />
-  </Provider>
-  , document.querySelector('.container'));
+// Take this component's generated HTML and put it on the page(DOM)
+ReactDOM.render(<App />, document.querySelector('.container')); //JSX tags to make an instance of App to render to the DOM
